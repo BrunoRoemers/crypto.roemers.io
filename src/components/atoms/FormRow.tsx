1974f2fe93a2c5086e2565dcc2851b1709808807
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-01-18 17:51:39 +0100

    Add FormRow component

---
+++ src/components/atoms/FormRow.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from "react";
+
+interface Props {
+  label?: string;
+  errorMessages?: string[];
+  children: ReactNode;
+  htmlFor?: string;
+}
+
+const FormRow = ({ label, errorMessages, children, htmlFor }: Props) => {
+  const hasLabel = label !== undefined;
+  const hasErrors = errorMessages !== undefined && errorMessages.length > 0;
+
+  const content = (
+    <>
+      <div>{children}</div>
+      {!hasErrors ? null : (
+        <ul className="text-red-600">
+          {errorMessages.map((errorMessage) => (
+            <li>{errorMessage}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+
+  const wrapperClx = "w-full flex px-2 py-1";
+
+  return hasLabel ? (
+    // NOTE: Since the component's children are rendered inside a label,
+    //       they are implicitly linked (no need for htmlFor and id).
+    //       However, the user of this component can provide an explicit htmlFor in case of conflict.
+    <label className={wrapperClx} htmlFor={htmlFor}>
+      <div className="w-1/3">{label}</div>
+      <div className="w-2/3">{content}</div>
+    </label>
+  ) : (
+    <div className={wrapperClx}>
+      <div className="w-2/3 ml-1/3">{content}</div>
+    </div>
+  );
+};
+
+export default FormRow;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    // NOTE: default config: https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+    extend: {
+      margin: {
+        "1/3": "33.333333%",
+      },
+    },
   },
   plugins: [],
-}
+};
